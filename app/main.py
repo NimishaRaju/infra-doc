40acd81cc3342535f99295b036337be825b14293
Nimisha Domma Raju Balasubramanyam Raju <nimisha.balasubramanyam.raju@oracle.com>
2026-08-17 08:09:50 +0100

    Adding initial project setup

---
+++ app/main.py
@@ -0,0 +1,8 @@
+#Define a function that prints the basic details on the app
+def startup():
+    print("Hello")
+    print("Welcome to infrasture health check platform")
+    print("application is starting now...")
+#Execute the startup function only if the file name is specified
+if __name__=='__main__':
+    startup()
