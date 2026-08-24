94d3146d63f8d548d1602c8027cc0216c0c74afd
Nimisha Domma Raju Balasubramanyam Raju <nimisha.balasubramanyam.raju@oracle.com>
2026-08-24 12:10:12 +0100

    improving code added few more checks and ruff tests

--- tests/test_health.py
+++ tests/test_health.py
@@ -1,4 +1,6 @@
 from app.healthcheck import ResourceHealth
+
+
 def test_health():
     t1=ResourceHealth("Milo")
     assert t1.cpu_health_check(40.2)==("overheated")
@@ -8,3 +10,11 @@ def test_health():
     assert t1.memory_health_check("ECC Errors")==("notok")
     assert t1.disk_health_check("Verified")==("healthy")
     assert t1.disk_health_check("Failing")==("unhealthy")
+    assert t1.bat_health_check("Normal")==("normal")
+    assert t1.bat_health_check("Failed")==("abnormal")
+    assert t1.net_health_check(0,0)==("ok")
+    assert t1.net_health_check(1,1)==("notok")
+    assert t1.sys_panic_check("panic")==("panic")
+    assert t1.sys_panic_check("")==("nopanic")
+    assert t1.thermal_lev_check(0,0,0)==("normal")
+    assert t1.thermal_lev_check(1,1,1)==("abnormal")
